bbc22ad1c567751e131e46fc2c9ac997e75864b6
navedev <naveen.d088@gmail.com>
2021-07-21 18:27:54 +0530

    Logs added

--- src/main/java/com/wipro/reglogin/controller/AuthController.java
+++ src/main/java/com/wipro/reglogin/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.wipro.reglogin.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,12 @@ import com.wipro.reglogin.model.RegistrationRequest;
 import com.wipro.reglogin.service.AuthService;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin
+@Slf4j
 public class AuthController {
 
 	@Autowired
@@ -23,12 +27,13 @@ public class AuthController {
 
 	@PostMapping(path = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> register(@Valid @RequestBody RegistrationRequest registrationReq) {
-
+		log.info("Request received for new Registration");
 		return authService.registerUser(registrationReq, false);
 	}
 
 	@PostMapping(path = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+		log.info("Login request received");
 		return authService.login(loginRequest);
 	}
 
